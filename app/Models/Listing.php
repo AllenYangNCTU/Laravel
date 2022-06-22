8610e6eafbe3d7a46dacde523d308a058f1f10e3
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-06-22 22:44:43 +0800

    database seeding & create an eloquent model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing
-{
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta voluptatibus laborum perspiciatis omnis maxime laboriosam similique repellendus. Aperiam tempore harum, nisi consequatur amet odio ut odit. Distinctio assumenda cupiditate autem'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta voluptatibus laborum perspiciatis omnis maxime laboriosam similique repellendus. Aperiam tempore harum, nisi consequatur amet odio ut odit. Distinctio assumenda cupiditate autem'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id)
-    {
-        $listings = self::all();
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
